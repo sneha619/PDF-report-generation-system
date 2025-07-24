9eebd9958b27cb921c8c86890f384f7a68d94ba9
sneha619 <sneha1995sarkar25@gmail.com>
2025-07-25 01:35:33 +0530

    Init

--- assessment-backend/render-build.sh
+++ assessment-backend/render-build.sh
@@ -1,19 +1,16 @@
 #!/bin/bash
-
-# Exit immediately if any command fails
 set -e
 
 # Step 1: Build frontend
-cd assessment-frontend
+cd ../assessment-frontend
 npm install
 npm run build
 
 # Step 2: Move build into backend
-cd ..
-mv -f assessment-frontend/build assessment-backend/
+mv -f build ../assessment-backend/
 
 # Step 3: Install backend dependencies
-cd assessment-backend
+cd ../assessment-backend
 npm install
 
 # Step 4: Install Puppeteer Chromium (required on Render)
